3bc564680533b0a412db6d4bfe715559b17712b6
Berlic <bsorrell3@gmail.com>
2021-08-03 21:33:25 -0400

    Added fetch for OMDB

--- assets/script.js
+++ assets/script.js
@@ -2,3 +2,22 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, options);
   });
+
+//.....Declaired Global Variable.....\\
+var keyOMDB="ea8bbe23";
+var titleSearch='Superman';
+
+// .....OMDB Info....
+// url is http://www.omdbapi.com/?t=[searchParam]&?apikey=[yourkey]&
+fetch("http://www.omdbapi.com/?t="+titleSearch+"&apikey="+keyOMDB)
+    .then(function(response){
+        if(response.status != 200){
+            console.log("nope")
+        };
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data);
+    });
+
+
