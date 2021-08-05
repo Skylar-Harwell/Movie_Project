53829f629bea2e41c6f972da6d4b029fa947de19
Skylar Harwell <swharwell@gmail.com>
2021-08-05 19:40:32 -0400

    Added button to fetch to grab user input

--- assets/script.js
+++ assets/script.js
@@ -1,28 +1,32 @@
-const movieInfoArea = document.querySelector(".movieInfoArea");
-const infoTitle = document.querySelector(".infoTitle");
-const movieInfo = document.querySelector(".movieInfo");
-const moviePoster = document.querySelector(".moviePoster");
-const actorList = document.querySelector(".actorList");
-const directorList = document.querySelector(".actorList");
-const search = document.querySelector(".search");
-const movieName = document.querySelector(".movieName");
-const submitBtn = document.querySelector(".buttons .submitBtn");
-const actorNames = document.querySelector(".buttons .actorNames");
+const movieInfoArea = document.querySelector("#movieInfoArea");
+const infoTitle = document.querySelector("#infoTitle");
+const movieInfo = document.querySelector("#movieInfo");
+const moviePoster = document.querySelector("#moviePoster");
+const actorList = document.querySelector("#actorList");
+const directorList = document.querySelector("#directorList");
+const search = document.querySelector("#search");
+var movieName = document.querySelector("#movieName");
+var submitBtn = document.querySelector("#submitBtn");
+const actorNames = document.querySelector("#actorNames");
 
+submitBtn.addEventListener('click', function() {
+  getMovieSearch(movieName.value);
+});
 
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     // var instances = M.Sidenav.init(elems, options);
   });
 
-
+var getMovieSearch = function (movieName) { 
 //.....Declaired Global Variable.....\\
 var keyOMDB="ea8bbe23";
-var titleSearch='Superman';
+var searchUrl = "http://www.omdbapi.com/?t="+movieName+"&apikey="+keyOMDB;
+// var titleSearch='Superman';
 
 // .....OMDB Info....
 // url is http://www.omdbapi.com/?t=[searchParam]&?apikey=[yourkey]&
-fetch("http://www.omdbapi.com/?t="+titleSearch+"&apikey="+keyOMDB)
+fetch(searchUrl)
     .then(function(response){
         if(response.status != 200){
             console.log("nope")
@@ -32,4 +36,4 @@ fetch("http://www.omdbapi.com/?t="+titleSearch+"&apikey="+keyOMDB)
     .then(function(data){
         console.log(data);
     });
-
+  };
